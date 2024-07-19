a89345c5ef73d70af6e931cbf3eac6c2d4c52c1f
이승현 <lsh96900410@gmail.com>
2024-07-20 01:07:43 +0900

    Stack

--- src/collection/deque/StackMain.java
+++ src/collection/deque/StackMain.java
@@ -3,6 +3,13 @@ package collection.deque;
 import java.util.Stack;
 
 public class StackMain {
+    /*
+        주의 사항 - Stack 클래스는 사용하지 말자
+
+        현재의 자바 Stack Class : 내부에서 Vector 자료 구조를 사용한다.
+        Vector : 자바1.0에 개발, 현재는 사용 X + 하휘 호환을 위해 존재한다.
+        Vector, Stack 사용 지양하기 -> Deque 사용 권장
+     */
 
     public static void main(String[] args) {
 
@@ -10,8 +17,16 @@ public class StackMain {
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        stack.push(4);
+        System.out.println(stack);
 
 
+        // 다음 꺼낼 요소 확인(꺼내지 않고 단순 조회만)
+        System.out.println("stack.peek() = " + stack.peek());
+
+        // 스택 요소 뽑기
+        System.out.println("stack.pop() = " + stack.pop());
+        System.out.println("stack.pop() = " + stack.pop());
+        System.out.println("stack.pop() = " + stack.pop());
+        System.out.println("stack = " + stack);
     }
 }
